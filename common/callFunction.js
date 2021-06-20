406f244dec1a61bbf9240c5a739a58fe904e698f
seho <1018715564@qq.com>
2021-06-20 15:52:03 +0800

    [feat]封装callfunction模块集中管理错误

---
+++ common/callFunction.js
@@ -0,0 +1,34 @@
+export default (params) => {
+	return new Promise(async (resolve) => {
+		const {
+			result
+		} = await uniCloud.callFunction({
+			name: params.name,
+			data: {
+				route: params.data.route,
+				method: params.data.method,
+				params: params.data.params,
+			},
+		});
+		let response = {
+			success: true,
+			data: result,
+		};
+		if (result.code) {
+			// 判断code是否存在于errorMessage对象中
+			if (result.code !== 0) {
+				response = {
+					success: false,
+				};
+				uni.hideLoading();
+				if (result.msg) {
+					uni.showToast({
+						title: result.msg,
+						icon: "none",
+					});
+				}
+			}
+		}
+		resolve(response);
+	});
+}
