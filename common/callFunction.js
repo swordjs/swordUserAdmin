ed09942dc4774af8c4650ed8fdbf84c281d8f472
seho <1018715564@qq.com>
2021-11-09 21:38:55 +0800

    🐞 fix: 兼容v1.1.0版本的请求方式

--- common/callFunction.js
+++ common/callFunction.js
@@ -1,34 +1,37 @@
 export default (params) => {
-	return new Promise(async (resolve) => {
-		const {
-			result
-		} = await uniCloud.callFunction({
-			name: params.name,
-			data: {
-				route: params.data.route,
-				method: params.data.method,
-				params: params.data.params,
-			},
-		});
-		let response = {
-			success: true,
-			data: result,
-		};
-		if (result.code) {
-			// 判断code是否存在于errorMessage对象中
-			if (result.code !== 0) {
-				response = {
-					success: false,
-				};
-				uni.hideLoading();
-				if (result.msg) {
-					uni.showToast({
-						title: result.msg,
-						icon: "none",
-					});
-				}
-			}
-		}
-		resolve(response);
-	});
-}
+  const {
+    VUE_APP_V1_TEST_API_HTTP_URL,
+    VUE_APP_V1_RELEASE_API_HTTP_URL,
+    UNI_PLATFORM,
+    NODE_ENV,
+  } = process.env;
+  const base_url =
+    NODE_ENV === "development"
+      ? VUE_APP_V1_TEST_API_HTTP_URL
+      : VUE_APP_V1_RELEASE_API_HTTP_URL;
+  const { method = "POST", params: _params = {}, route } = params;
+
+  // uni-id-token
+  const token = uni.getStorageSync("uni_id_token");
+  const id = uni.getStorageSync("uid");
+
+  return new Promise(async (resolve) => {
+    uni.request({
+      url: `${base_url}${route}`,
+      method,
+      data: _params,
+      header: {
+        "uni-id-token": token,
+        uni_id: id,
+        "sword-platform": UNI_PLATFORM,
+      },
+      success: (res) => {
+        resolve({
+          success: true,
+          data: res.data,
+        });
+      },
+      fail: () => {},
+    });
+  });
+};
