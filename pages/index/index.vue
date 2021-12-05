b0e5945b4efdc9f40f1117a65216b7b9a75f281a
seho <1018715564@qq.com>
2021-12-05 17:48:24 +0800

    🌈 style: 修改展示文案

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,29 +1,3 @@
 <template>
-    <scroll-view style="padding: 15px;box-sizing: border-box;">
-        首页 dashbord正在建设中...
-		<!-- #ifndef H5 -->
-		<fix-window />
-		<!-- #endif -->
-    </scroll-view>
+	<div>尽情期待，下个版本 by Ucharts</div>
 </template>
-
-<script>
-	import { version } from '../../package.json'
-    export default {
-        data() {
-            return {
-				adminVersion: version
-			}
-        },
-        onLoad() {},
-        methods: {}
-    }
-</script>
-
-<style>
-	/* #ifndef H5 */
-	page {
-		padding-top: 85px;
-	}
-	/* #endif */
-</style>
