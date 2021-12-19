53fde80b5f2960ae61b01897edd07fe685cea16a
seho <1018715564@qq.com>
2021-12-19 16:03:07 +0800

    🎈 perf: 优化部分代码结构

--- pages/article/list.vue
+++ pages/article/list.vue
@@ -7,12 +7,8 @@
 			</view>
 			<view class="uni-group">
 				<input class="uni-search" type="text" v-model="query" @confirm="search" placeholder="文章标题" />
-				<button class="uni-button" type="default" size="mini" @click="search">
-					搜索
-				</button>
-				<button @click="navigateTo('./add')" size="mini" class="uni-button" type="primary">
-					发布新文章
-				</button>
+				<button class="uni-button" type="default" size="mini" @click="search">搜索</button>
+				<button @click="navigateTo('./add')" size="mini" class="uni-button" type="primary">发布新文章</button>
 			</view>
 		</view>
 		<view class="uni-container">
@@ -21,11 +17,20 @@
 				<a-tab-pane key="pass" tab="已审核"></a-tab-pane>
 				<a-tab-pane key="reject" tab="已拒绝"></a-tab-pane>
 			</a-tabs>
-			<unicloud-db ref="dataQuery" @load="onqueryload" collection="article,questionTag,uni-id-users"
-				:options="options" field="title,content,tagID{name},userID{nickname},createDate"
+			<unicloud-db
+				ref="dataQuery"
+				@load="onqueryload"
+				collection="article,questionTag,uni-id-users"
+				:options="options"
+				field="title,content,tagID{name},userID{nickname},createDate"
 				:where="(where !== '' ? where + '&&' : '') + `state == '${state}' && userID._id == '${uid}'`"
-				page-data="replace" :orderby="orderby" :getcount="true" :page-size="options.pageSize"
-				:page-current="options.pageCurrent" v-slot:default="{ data, pagination, loading, error }">
+				page-data="replace"
+				:orderby="orderby"
+				:getcount="true"
+				:page-size="options.pageSize"
+				:page-current="options.pageCurrent"
+				v-slot:default="{ data, pagination, loading, error }"
+			>
 				<uni-table :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe>
 					<uni-tr>
 						<uni-th width="250" align="center">id</uni-th>
@@ -38,33 +43,43 @@
 					<uni-tr v-for="(item, index) in data" :key="index">
 						<uni-td align="center">{{ item._id }}</uni-td>
 						<uni-td align="center">{{ item.tagID.map(t => t.name).join(",") }}</uni-td>
-						<uni-td align="center">
-							{{ item.title }}
-						</uni-td>
+						<uni-td align="center">{{ item.title }}</uni-td>
 						<uni-td align="center">
 							<div class="articleContent">{{ item.content === "" ? "内容暂无" : item.content }}</div>
 						</uni-td>
-						<uni-td align="center"> {{ item.createDate | formatTime }}</uni-td>
+						<uni-td align="center">{{ item.createDate | formatTime }}</uni-td>
 						<uni-td align="center" v-if="state === 'onlist'">
 							<view class="uni-group">
-								<button size="mini" @click="navigateTo('./detail?id=' + item._id, false)"
-									class="uni-button" type="primary">
-									修改
-								</button>
+								<button
+									size="mini"
+									@click="navigateTo('./detail?id=' + item._id, false)"
+									class="uni-button"
+									type="primary"
+								>修改</button>
 							</view>
 						</uni-td>
 					</uni-tr>
 				</uni-table>
 				<view class="uni-pagination-box">
-					<picker class="select-picker" mode="selector" :value="pageSizeIndex" :range="pageSizeOption"
-						@change="changeSize">
+					<picker
+						class="select-picker"
+						mode="selector"
+						:value="pageSizeIndex"
+						:range="pageSizeOption"
+						@change="changeSize"
+					>
 						<button type="default" size="mini" :plain="true">
 							<text>{{ pageSizeOption[pageSizeIndex] }} 条/页</text>
 							<uni-icons class="select-picker-icon" type="arrowdown" size="12" color="#999"></uni-icons>
 						</button>
 					</picker>
-					<uni-pagination show-icon :page-size="pagination.size" v-model="pagination.current"
-						:total="pagination.count" @change="onPageChanged" />
+					<uni-pagination
+						show-icon
+						:page-size="pagination.size"
+						v-model="pagination.current"
+						:total="pagination.count"
+						@change="onPageChanged"
+					/>
 				</view>
 			</unicloud-db>
 		</view>
@@ -75,102 +90,102 @@
 </template>
 
 <script>
-	const db = uniCloud.database();
-	// 表查询配置
-	const dbOrderBy = "create_date desc";
-	const dbSearchFields = ["title"]; // 支持模糊搜索的字段列表
-	// 分页配置
-	const pageSize = 20;
-	const pageCurrent = 1;
-	const uid = uni.getStorageSync("uid");
+const db = uniCloud.database();
+// 表查询配置
+const dbOrderBy = "create_date desc";
+const dbSearchFields = ["title"]; // 支持模糊搜索的字段列表
+// 分页配置
+const pageSize = 20;
+const pageCurrent = 1;
+const uid = uni.getStorageSync("uid");
 
-	export default {
-		data() {
-			return {
-				query: "",
-				where: "",
-				state: "onlist",
-				orderby: dbOrderBy,
-				options: {
-					pageSize,
-					pageCurrent,
-				},
-				selectedIndexs: [], //批量选中的项
-				pageSizeIndex: 1,
-				pageSizeOption: [1, 20, 50, 100, 500],
-				uid
-			};
-		},
-		watch: {
-			pageSizeIndex: {
-				immediate: true,
-				handler(val, old) {
-					this.options.pageSize = this.pageSizeOption[val];
-					this.options.pageCurrent = 1;
-				},
+export default {
+	data() {
+		return {
+			query: "",
+			where: "",
+			state: "onlist",
+			orderby: dbOrderBy,
+			options: {
+				pageSize,
+				pageCurrent,
+			},
+			selectedIndexs: [], //批量选中的项
+			pageSizeIndex: 1,
+			pageSizeOption: [1, 20, 50, 100, 500],
+			uid
+		};
+	},
+	watch: {
+		pageSizeIndex: {
+			immediate: true,
+			handler(val, old) {
+				this.options.pageSize = this.pageSizeOption[val];
+				this.options.pageCurrent = 1;
 			},
 		},
-		methods: {
-			onqueryload(data) {
-				for (var i = 0; i < data.length; i++) {
-					let item = data[i];
-					item.create_date = this.$formatDate(item.create_date);
-				}
-				this.expData = data; //仅导出当前页
-			},
-			changeSize(e) {
-				this.pageSizeIndex = e.detail.value;
-			},
-			getWhere() {
-				const query = this.query.trim();
-				if (!query) {
-					return "";
-				}
-				const queryRe = `/${query}/i`;
-				return dbSearchFields
-					.map((name) => queryRe + ".test(" + name + ")")
-					.join(" || ");
-			},
-			search() {
-				const newWhere = this.getWhere();
-				const isSameWhere = newWhere === this.where;
-				this.where = newWhere;
-				if (isSameWhere) {
-					// 相同条件时，手动强制刷新
-					this.loadData();
-				}
-			},
-			loadData(clear = true) {
-				this.$refs.dataQuery.loadData({
-					clear,
-				});
-			},
-			onPageChanged(e) {
-				this.options.pageCurrent = e.current;
-			},
-			navigateTo(url, clear) {
-				// clear 表示刷新列表时是否清除当前页码，true 表示刷新并回到列表第 1 页，默认为 true
-				uni.navigateTo({
-					url,
-					events: {
-						refreshData: () => {
-							this.loadData(clear);
-						},
+	},
+	methods: {
+		onqueryload(data) {
+			for (var i = 0; i < data.length; i++) {
+				let item = data[i];
+				item.create_date = this.$formatDate(item.create_date);
+			}
+			this.expData = data; //仅导出当前页
+		},
+		changeSize(e) {
+			this.pageSizeIndex = e.detail.value;
+		},
+		getWhere() {
+			const query = this.query.trim();
+			if (!query) {
+				return "";
+			}
+			const queryRe = `/${query}/i`;
+			return dbSearchFields
+				.map((name) => queryRe + ".test(" + name + ")")
+				.join(" || ");
+		},
+		search() {
+			const newWhere = this.getWhere();
+			const isSameWhere = newWhere === this.where;
+			this.where = newWhere;
+			if (isSameWhere) {
+				// 相同条件时，手动强制刷新
+				this.loadData();
+			}
+		},
+		loadData(clear = true) {
+			this.$refs.dataQuery.loadData({
+				clear,
+			});
+		},
+		onPageChanged(e) {
+			this.options.pageCurrent = e.current;
+		},
+		navigateTo(url, clear) {
+			// clear 表示刷新列表时是否清除当前页码，true 表示刷新并回到列表第 1 页，默认为 true
+			uni.navigateTo({
+				url,
+				events: {
+					refreshData: () => {
+						this.loadData(clear);
 					},
-				});
-			},
-			handleStateChange(type) {
-				this.state = type;
-			},
+				},
+			});
 		},
-	};
+		handleStateChange(type) {
+			this.state = type;
+		},
+	},
+};
 </script>
 
 <style scoped>
-	.articleContent {
-		width: 250px;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-	}
+.articleContent {
+	width: 250px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+}
 </style>
--- pages/openApi/list.vue
+++ pages/openApi/list.vue
@@ -7,12 +7,14 @@
 			</view>
 			<view class="uni-group">
 				<div style="margin-right: 20px;">
-					<unicloud-db v-slot:default="{data, loading, error, options}" collection="uni-id-users"
-						field="openApiRequestNumber" :where="{
-						_id: '60841eb4b09d4a000177567c'
-					}">
-						{{(!error && data.length > 0) ? `剩余请求次数(每月重置): ${data[0].openApiRequestNumber || 200}次` : '--'}}
-					</unicloud-db>
+					<unicloud-db
+						v-slot:default="{ data, loading, error, options }"
+						collection="uni-id-users"
+						field="openApiRequestNumber"
+						:where="{
+							_id: '60841eb4b09d4a000177567c'
+						}"
+					>{{ (!error && data.length > 0) ? `剩余请求次数(每月重置): ${data[0].openApiRequestNumber || 200}次` : '--' }}</unicloud-db>
 				</div>
 				<input class="uni-search" type="text" v-model="query" @confirm="search" placeholder="请输入搜索内容" />
 				<button class="uni-button" type="default" size="mini" @click="search">搜索</button>
@@ -20,13 +22,29 @@
 			</view>
 		</view>
 		<view class="uni-container">
-			<unicloud-db ref="udb" collection="openApi" field="name,remark" :where="{
-				...where,
-				state: 'open'
-			}" page-data="replace" :orderby="orderby" :getcount="true" :page-size="options.pageSize"
-				:page-current="options.pageCurrent" v-slot:default="{ data, pagination, loading, error, options }"
-				:options="options">
-				<uni-table :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe type="selection">
+			<unicloud-db
+				ref="udb"
+				collection="openApi"
+				field="name,remark"
+				:where="{
+					...where,
+					state: 'open'
+				}"
+				page-data="replace"
+				:orderby="orderby"
+				:getcount="true"
+				:page-size="options.pageSize"
+				:page-current="options.pageCurrent"
+				v-slot:default="{ data, pagination, loading, error, options }"
+				:options="options"
+			>
+				<uni-table
+					:loading="loading"
+					:emptyText="error.message || '没有更多数据'"
+					border
+					stripe
+					type="selection"
+				>
 					<uni-tr>
 						<uni-th align="center">api名称</uni-th>
 						<uni-th align="center">备注</uni-th>
@@ -38,15 +56,19 @@
 
 						<uni-td align="center">
 							<view class="uni-group">
-								<button @click="handleUrl(item._id)" class="uni-button" size="mini"
-									type="primary">复制URL</button>
+								<button @click="handleUrl(item._id)" class="uni-button" size="mini" type="primary">复制URL</button>
 							</view>
 						</uni-td>
 					</uni-tr>
 				</uni-table>
 				<view class="uni-pagination-box">
-					<uni-pagination show-icon :page-size="pagination.size" v-model="pagination.current"
-						:total="pagination.count" @change="onPageChanged" />
+					<uni-pagination
+						show-icon
+						:page-size="pagination.size"
+						v-model="pagination.current"
+						:total="pagination.count"
+						@change="onPageChanged"
+					/>
 				</view>
 			</unicloud-db>
 		</view>
@@ -54,96 +76,95 @@
 </template>
 
 <script>
-	import uniCopy from '@/js_sdk/xb-copy/uni-copy.js'
+import uniCopy from '@/js_sdk/xb-copy/uni-copy.js'
 
-	const db = uniCloud.database()
-	// 表查询配置
-	const dbOrderBy = '' // 排序字段
-	const dbSearchFields = ["name", "remark"] // 模糊搜索字段，支持模糊搜索的字段列表
-	// 分页配置
-	const pageSize = 20
-	const pageCurrent = 1
+const db = uniCloud.database()
+// 表查询配置
+const dbOrderBy = '' // 排序字段
+const dbSearchFields = ["name", "remark"] // 模糊搜索字段，支持模糊搜索的字段列表
+// 分页配置
+const pageSize = 20
+const pageCurrent = 1
 
-	export default {
-		data() {
-			return {
-				query: '',
-				where: '',
-				orderby: dbOrderBy,
-				selectedIndexs: [],
-				options: {
-					pageSize,
-					pageCurrent,
+export default {
+	data() {
+		return {
+			query: '',
+			where: '',
+			orderby: dbOrderBy,
+			selectedIndexs: [],
+			options: {
+				pageSize,
+				pageCurrent,
+			},
+			imageStyles: {
+				width: 64,
+				height: 64
+			}
+		}
+	},
+	methods: {
+		handleUrl(id) {
+			// 获取token
+			const token = uni.getStorageSync("uni_id_token");
+			// 这里的url是release环境的云函数url
+			uniCopy({
+				content: `https://86dc45ba-28e8-4734-a880-bbf700b08cf9.bspapp.com/http/openapi?token=${token}&apiID=${id}`,
+				success: () => {
+					uni.showToast({
+						title: "URL复制成功",
+						icon: 'none'
+					})
 				},
-				imageStyles: {
-					width: 64,
-					height: 64
+				error: (e) => {
+					uni.showToast({
+						title: e,
+						icon: 'none',
+						duration: 3000,
+					})
 				}
+			});
+		},
+		getWhere() {
+			const query = this.query.trim()
+			if (!query) {
+				return ''
+			}
+			const queryRe = new RegExp(query, 'i')
+			return dbSearchFields.map(name => queryRe + '.test(' + name + ')').join(' || ')
+		},
+		search() {
+			const newWhere = this.getWhere()
+			const isSameWhere = newWhere === this.where
+			this.where = newWhere
+			if (isSameWhere) { // 相同条件时，手动强制刷新
+				this.loadData()
 			}
 		},
-		methods: {
-			handleUrl(id) {
-				// 获取token
-				const token = uni.getStorageSync("uni_id_token");
-				// 这里的url是release环境的云函数url
-				uniCopy({
-					content: `https://86dc45ba-28e8-4734-a880-bbf700b08cf9.bspapp.com/http/openapi?token=${token}&apiID=${id}`,
-					success: () => {
-						uni.showToast({
-							title: "URL复制成功",
-							icon: 'none'
-						})
-					},
-					error: (e) => {
-						uni.showToast({
-							title: e,
-							icon: 'none',
-							duration: 3000,
-						})
+		loadData(clear = true) {
+			this.$refs.udb.loadData({
+				clear
+			})
+		},
+		onPageChanged(e) {
+			this.$refs.udb.loadData({
+				current: e.current
+			})
+		},
+		navigateTo(url, clear) {
+			// clear 表示刷新列表时是否清除页码，true 表示刷新并回到列表第 1 页，默认为 true
+			uni.navigateTo({
+				url,
+				events: {
+					refreshData: () => {
+						this.loadData(clear)
 					}
-				});
-			},
-			getWhere() {
-				const query = this.query.trim()
-				if (!query) {
-					return ''
 				}
-				const queryRe = new RegExp(query, 'i')
-				return dbSearchFields.map(name => queryRe + '.test(' + name + ')').join(' || ')
-			},
-			search() {
-				const newWhere = this.getWhere()
-				const isSameWhere = newWhere === this.where
-				this.where = newWhere
-				if (isSameWhere) { // 相同条件时，手动强制刷新
-					this.loadData()
-				}
-			},
-			loadData(clear = true) {
-				this.$refs.udb.loadData({
-					clear
-				})
-			},
-			onPageChanged(e) {
-				this.$refs.udb.loadData({
-					current: e.current
-				})
-			},
-			navigateTo(url, clear) {
-				// clear 表示刷新列表时是否清除页码，true 表示刷新并回到列表第 1 页，默认为 true
-				uni.navigateTo({
-					url,
-					events: {
-						refreshData: () => {
-							this.loadData(clear)
-						}
-					}
-				})
-			},
-		}
+			})
+		},
 	}
+}
 </script>
 
 <style lang="scss">
-
 </style>
